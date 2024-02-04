9fad33a9db7292f8e3b62514e9272c0da8363ab4
september-a <september.abbott@gmail.com>
2024-02-04 17:12:09 -0600

    implemented simple AJAX

--- student_server.php
+++ student_server.php
@@ -1,6 +1,20 @@
 <?php 
- 
- $name = $_POST['studentData'];
- echo $name;
+    
+    $nameArr = array();
+
+    session_start(); // Start or resume the session
+
+    $nameArr = isset($_SESSION["nameArr"]) ? $_SESSION["nameArr"] : array();
+
+    if (isset($_REQUEST["fname"])) {
+        $mydata = $_REQUEST["fname"];
+        array_push($nameArr, $mydata);
+        $_SESSION["nameArr"] = $nameArr; // Store the updated array in the session
+    }
+
+    if (isset($_REQUEST["lastValue"])) {
+        $lastValue = end($nameArr);
+        echo $lastValue;
+    }
   
 ?>
