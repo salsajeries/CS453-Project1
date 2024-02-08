74c927bba1e7dbd539a7811a235d84fba98c5b89
Salwa Jeries <annette.jeries@gmail.com>
2024-02-07 20:03:41 -0600

    It's done pretty much

--- simple_serverB.php
+++
@@ -1,5 +0,0 @@
-<?php 
- 
-
-  print "<p>stuff from other server</p> ";
-?>
--- test_server.php
+++ test_server.php
@@ -106,9 +106,10 @@ class Student {
   public $course;
   public $book1;
   public $book2;
+  public $compliant = false;
   
   // Methods
-  public function __construct($name, $course, $book1, $book2) {
+  public function __construct($name, $course, $book1, $book2, $compliant = false) {
       $this->set_name($name);
       $this->set_course($course);
       $this->set_book1($book1);
@@ -132,6 +133,10 @@ class Student {
       $this->book2 = $book;
   }
 
+  public function set_compliant($compliant) {
+    $this->compliant = $compliant;
+}
+
   public function get_name() {
       return $this->name;
   }
@@ -147,10 +152,16 @@ class Student {
   public function get_book2() {
       return $this->book2;
   }
+
+  public function get_compliant() {
+    return $this->compliant;
+}
 }
 
 // Global array to store course objects
 $courseArray = isset($_SESSION["courseArray"]) ? $_SESSION["courseArray"] : array();
+// Global array to store student objects
+$studentArray = isset($_SESSION["studentArray"]) ? $_SESSION["studentArray"] : array();
 
 // Function to process course request
 function processCourseRequest($courseName, $book1Data, $book2Data) {
@@ -165,31 +176,107 @@ function processCourseRequest($courseName, $book1Data, $book2Data) {
 
     // Add the course object to the global array
     global $courseArray;
-    //$courseArray[] = $course;
     array_push($courseArray, $course);
+    $_SESSION["courseArray"] = $courseArray;
 
     // Convert the course object to JSON
     $courseJson = json_encode($course);
 
     // Return the course object to the HTML page
-    //echo $courseJson;
-    $_SESSION["courseArray"] = $courseArray;
-    echo json_encode($courseArray);
+    echo $courseJson;
 }
 
-// Function to process another type of request
-function processAnotherRequest($param1, $param2) {
-    // Perform actions based on the parameters
-    // ...
+function processStudentRequest($studentName, $courseName, $book1Data, $book2Data){
+    // Put book1 data in a Book object
+    $book1 = new Book($book1Data['title'], $book1Data['publisher'], $book1Data['edition'], $book1Data['printDate']);
 
-    // Return the result
-    echo "Result for another request";
+    // Put book2 data in a Book object
+    $book2 = new Book($book2Data['title'], $book2Data['publisher'], $book2Data['edition'], $book2Data['printDate']);
+
+    $student = new Student($studentName, $courseName, $book1, $book2);
+
+    // Add the student object to the global array
+    global $studentArray;
+    array_push($studentArray, $student);
+    $_SESSION["studentArray"] = $studentArray;
+
+    // Convert the student object to JSON
+    $studentJson = json_encode($student);
+    
+    echo $studentJson;
 }
 
 // Function to get all course objects
 function getAllCourses() {
     global $courseArray;
+    $_SESSION["courseArray"] = $courseArray;
     return json_encode($courseArray);
+}
+
+function getAllStudents() {
+    global $studentArray;
+    $_SESSION["studentArray"] = $studentArray;
+    return json_encode($studentArray);
+}
+
+function validateTextbooks() {
+  // Student loop
+  global $studentArray;
+  global $courseArray;
+
+  foreach ($studentArray as $student) {
+    
+    $student->compliant = false;    // Default to false
+    
+    foreach ($courseArray as $course) {
+      // Compare student course with current course
+      if ($student->get_course == $course->get_name) {
+        $validateBook1 = false;
+        $validateBook2 = false;
+
+        // if course.book1 is not NULL
+        if ($course->book1->get_title() != "") {
+          // if (student.book1.getTitle == course.book1.getTitle)
+          if ($student->book1->get_title() == $course->book1->get_title()) {
+            echo "can you see me";
+            if ($student->book1 == $course->book1)
+              $validateBook1 = true;
+          }
+          // else if (student.book2.getTitle == course.book1.getTitle)
+          else if ($student->book2->get_title() == $course->book1->get_title()) {
+            if ($student->book2 == $course->book1)
+              $validateBook1 = true;
+          }
+        }
+        else {
+          $validateBook1 = true;
+        }
+        
+        // if course.book2 is not NULL
+        if ($course->book2->get_title() != "") {
+
+          // if (student.book1.getTitle == course.book1.getTitle)
+          if ($student->book1->get_title() == $course->book2->get_title()) {
+            if ($student->book1 == $course->book2)
+              $validateBook2 = true;
+          }
+          // else if (student.book2.getTitle == course.book1.getTitle)
+          else if ($student->book2->get_title() == $course->book2->get_title()) {
+            if ($student->book2 == $course->book2)
+              $validateBook2 = true;
+          }
+        }
+        else {
+          $validateBook2 = true;
+        }
+          
+        if ($validateBook1 && $validateBook2) {
+          $student->set_compliant(true);
+          $_SESSION["studentArray"] = $studentArray;
+        }
+      }
+    }
+  }
 }
 
 // Determine the type of request
@@ -218,13 +305,26 @@ switch ($requestType) {
         processCourseRequest($courseName, $book1Data, $book2Data);
         break;
 
-    case 'anotherRequest':
-        // Read the request for another type
-        $param1 = isset($_GET['param1']) ? $_GET['param1'] : '';
-        $param2 = isset($_GET['param2']) ? $_GET['param2'] : '';
+    case 'addStudent':
+        // Read the request for student
+        $studentName = isset($_GET['name']) ? $_GET['name'] : '';
+        $courseName = isset($_GET['course']) ? $_GET['course'] : '';
+        $book1Data = array(
+            'title' => $_GET['book1N'],
+            'publisher' => $_GET['book1P'],
+            'edition' => $_GET['book1E'],
+            'printDate' => $_GET['pd1']
+        );
 
-        // Call the function for processing another type of request
-        processAnotherRequest($param1, $param2);
+        $book2Data = array(
+            'title' => $_GET['book2N'],
+            'publisher' => $_GET['book2P'],
+            'edition' => $_GET['book2E'],
+            'printDate' => $_GET['pd2']
+        );
+
+        // Call the function for processing student request
+        processStudentRequest($studentName, $courseName, $book1Data, $book2Data);
         break;
 
     case 'getAllCourses':
@@ -232,10 +332,24 @@ switch ($requestType) {
         echo getAllCourses();
         break;
 
-    // Add more cases for different types of requests as needed
-    case 'clear':
+    case 'getAllStudents':
+        // Call the function to get all student objects
+        echo getAllStudents();
+        break;
+
+    case 'clearCourses':
         // Clear session
         unset($_SESSION["courseArray"]);
+        break;
+
+    case 'clearStudents':
+        // Clear session
+        unset($_SESSION["studentArray"]);
+        break;
+
+    case 'validate':
+        // Validate student textbooks 
+        validateTextbooks();
         break;
 
     default:
